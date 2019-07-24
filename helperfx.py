69605590a56b765973621702ccfe2145ccfd6450
Jaime McCutcheon <jem64@le.ac.uk>
2019-07-24 10:20:51 +0100

    Added initial notebook and helpfx

---
+++ helperfx.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 24 09:35:10 2019
+
+@author: James Rig
+"""
+
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+def medfilereader(filename, varsToExtract = 'all',
+                  sessionToExtract = 1,
+                  verbose = False,
+                  remove_var_header = False):
+    if varsToExtract == 'all':
+        numVarsToExtract = np.arange(0,26)
+    else:
+        numVarsToExtract = [ord(x)-97 for x in varsToExtract]
+    
+    f = open(filename, 'r')
+    f.seek(0)
+    filerows = f.readlines()[8:]
+    datarows = [isnumeric(x) for x in filerows]
+    matches = [i for i,x in enumerate(datarows) if x == 0.3]
+    if sessionToExtract > len(matches):
+        print('Session ' + str(sessionToExtract) + ' does not exist.')
+    if verbose == True:
+        print('There are ' + str(len(matches)) + ' sessions in ' + filename)
+        print('Analyzing session ' + str(sessionToExtract))
+    
+    varstart = matches[sessionToExtract - 1]
+    medvars = [[] for n in range(26)]
+    
+    k = int(varstart + 27)
+    for i in range(26):
+        medvarsN = int(datarows[varstart + i + 1])
+        
+        medvars[i] = datarows[k:k + int(medvarsN)]
+        k = k + medvarsN
+        
+    if remove_var_header == True:
+        varsToReturn = [medvars[i][1:] for i in numVarsToExtract]
+    else:
+        varsToReturn = [medvars[i] for i in numVarsToExtract]
+
+    if np.shape(varsToReturn)[0] == 1:
+        varsToReturn = varsToReturn[0]
+    return varsToReturn
+
+def isnumeric(s):
+    try:
+        x = float(s)
+        return x
+    except ValueError:
+        return float('nan')
+    
+def lickCalc(licks, offset = [], burstThreshold = 0.25, runThreshold = 10, 
+             binsize=60, histDensity = False, adjustforlonglicks='none',
+             minburstlength=1):
+    # makes dictionary of data relating to licks and bursts
+    if type(licks) != np.ndarray or type(offset) != np.ndarray:
+        try:
+            licks = np.array(licks)
+            offset = np.array(offset)
+        except:
+            print('Licks and offsets need to be arrays and unable to easily convert.')
+            return
+
+    lickData = {}
+
+    if len(offset) > 0:        
+        lickData['licklength'] = offset - licks[:len(offset)]
+        lickData['longlicks'] = [x for x in lickData['licklength'] if x > 0.3]
+    else:
+        lickData['licklength'] = []
+        lickData['longlicks'] = []
+    
+    if adjustforlonglicks != 'none':
+        if len(lickData['longlicks']) == 0:
+            print('No long licks to adjust for.')
+        else:
+            lickData['median_ll'] = np.median(lickData['licklength'])
+            lickData['licks_adj'] = int(np.sum(lickData['licklength'])/lickData['median_ll'])
+            if adjustforlonglicks == 'interpolate':
+                licks_new = []
+                for l, off in zip(licks, offset):
+                    x = l
+                    while x < off - lickData['median_ll']:
+                        licks_new.append(x)
+                        x = x + lickData['median_ll']
+                licks = licks_new
+        
+    lickData['licks'] = licks
+    lickData['ilis'] = np.diff(np.concatenate([[0], licks]))
+    lickData['shilis'] = [x for x in lickData['ilis'] if x < burstThreshold]
+    lickData['freq'] = 1/np.mean([x for x in lickData['ilis'] if x < burstThreshold])
+    lickData['total'] = len(licks)
+    
+    # Calculates start, end, number of licks and time for each BURST 
+    lickData['bStart'] = [val for i, val in enumerate(lickData['licks']) if (val - lickData['licks'][i-1] > burstThreshold)]  
+    lickData['bInd'] = [i for i, val in enumerate(lickData['licks']) if (val - lickData['licks'][i-1] > burstThreshold)]
+    lickData['bEnd'] = [lickData['licks'][i-1] for i in lickData['bInd'][1:]]
+    lickData['bEnd'].append(lickData['licks'][-1])
+
+    lickData['bLicks'] = np.diff(lickData['bInd'] + [len(lickData['licks'])])
+    
+    # calculates which bursts are above the minimum threshold
+    inds = [i for i, val in enumerate(lickData['bLicks']) if val>minburstlength-1]
+    
+    lickData['bLicks'] = removeshortbursts(lickData['bLicks'], inds)
+    lickData['bStart'] = removeshortbursts(lickData['bStart'], inds)
+    lickData['bEnd'] = removeshortbursts(lickData['bEnd'], inds)
+        
+    lickData['bTime'] = np.subtract(lickData['bEnd'], lickData['bStart'])
+    lickData['bNum'] = len(lickData['bStart'])
+    if lickData['bNum'] > 0:
+        lickData['bMean'] = np.nanmean(lickData['bLicks'])
+        lickData['bMean-first3'] = np.nanmean(lickData['bLicks'][:3])
+    else:
+        lickData['bMean'] = 0
+        lickData['bMean-first3'] = 0
+    
+    lickData['bILIs'] = [x for x in lickData['ilis'] if x > burstThreshold]
+
+    # Calculates start, end, number of licks and time for each RUN
+    lickData['rStart'] = [val for i, val in enumerate(lickData['licks']) if (val - lickData['licks'][i-1] > runThreshold)]  
+    lickData['rInd'] = [i for i, val in enumerate(lickData['licks']) if (val - lickData['licks'][i-1] > runThreshold)]
+    lickData['rEnd'] = [lickData['licks'][i-1] for i in lickData['rInd'][1:]]
+    lickData['rEnd'].append(lickData['licks'][-1])
+
+    lickData['rLicks'] = np.diff(lickData['rInd'] + [len(lickData['licks'])])    
+    lickData['rTime'] = np.subtract(lickData['rEnd'], lickData['rStart'])
+    lickData['rNum'] = len(lickData['rStart'])
+
+    lickData['rILIs'] = [x for x in lickData['ilis'] if x > runThreshold]
+    try:
+        lickData['hist'] = np.histogram(lickData['licks'][1:], 
+                                    range=(0, 3600), bins=int((3600/binsize)),
+                                          density=histDensity)[0]
+    except TypeError:
+        print('Problem making histograms of lick data')
+        
+    return lickData
+
+def removeshortbursts(data, inds):
+    data = [data[i] for i in inds]
+    return data
